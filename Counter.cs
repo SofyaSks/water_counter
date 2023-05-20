0f7997aaabb3eeb53adf63e0fba11df78671c9d1
sks <ssurovneva@mail.ru>
2023-05-20 19:30:17 +0300

    Add project files.

---
+++ Counter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace water_counter
+{
+    internal class Counter
+    {
+        // public DateTime date = new DateTime();
+        public string Maker { get; set; }
+        public string Model { get; set; }
+        public int SerialID { get; set; }
+        public string lastCheck { /*get { return date.ToShortDateString(); } set { }*/ get; set; }
+        public bool hotWater { get; set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace water_counter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Counter c = new Counter();
+            // string str = c.date.ToShortDateString();
+            // lastCheckTB.Text = str;
+
+            this.DataContext = new Counter
+            {
+                Maker = "Water.Co",
+                Model = "Great",
+                SerialID = 116,
+                lastCheck = "12.09.2023",
+                hotWater = true
+            };
+
+            
+
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"You made following changes:\nПроизводитель: { MakerTB.Text }\nМодель: {ModelTB.Text}\n" +
+                $"Серийный номер: {IdTB.Text}\nДата последней проверки: {lastCheckTB.Text}\nПредназначен для горячей воды: {hotWaterChB.IsChecked}");
+
+        }
+    }
+}
